8834cbe60d679b5b1b5b9b5ffa1b12c538b7ca89
Ed4n <edgardo.avalencia@gmail.com>
2023-11-11 19:10:45 -0600

    edit question logic worked

--- src/components/questions/Question.jsx
+++ src/components/questions/Question.jsx
@@ -2,10 +2,41 @@ import React, { useRef, useState } from "react";
 import { ConfirmationActions } from "./ConfirmationActions";
 export function Question({ i, question }) {
   const [enableEdit, setEnableEdit] = useState(false);
+  const [questionEdit, setQuestionEdit] = useState({
+    _id: "",
+    question: question.question,
+    answer: false,
+  });
+
   const focusEditing = useRef(null);
 
-  const toggleEnableEdit = () => {
+  function patchQuestionFetch() {
+    const url = "https://enigmus-api.vercel.app/api/v1/questions";
+
+    fetch(url, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newQuestion),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then((responseData) => {
+        console.log("POST request successful:", responseData);
+      })
+      .catch((error) => {
+        console.error("POST request error:", error);
+      });
+  }
+
+  const toggleEnableEdit = (id) => {
     setEnableEdit(true);
+    console.log(id);
     try {
       setTimeout(() => {
         focusEditing.current.focus();
@@ -17,6 +48,15 @@ export function Question({ i, question }) {
 
   const handleCancelEdit = () => {
     setEnableEdit(false);
+  };
+
+  const handleChange = (e) => {
+    const { value } = e.target;
+    console.log(questionEdit);
+    setQuestionEdit({
+      ...questionEdit,
+      question: value,
+    });
   };
 
   return (
@@ -35,7 +75,7 @@ export function Question({ i, question }) {
         }
       >
         <button
-          onClick={toggleEnableEdit}
+          onClick={() => toggleEnableEdit(questionEdit._id)}
           className={
             (enableEdit ? "ml-[16%]" : " ") +
             " w-full h-full  pl-5 py-3 text-left flex items-start justify-start"
@@ -44,9 +84,10 @@ export function Question({ i, question }) {
           {enableEdit ? (
             <input
               type="text"
+              onChange={handleChange}
               ref={focusEditing}
-              value={question.question}
-              className="flex w-[78%] bg-transparent  pl-5 outline-none"
+              value={questionEdit.question}
+              className="flex w-[78%] bg-transparent  pl-5 outline-none placeholder:text-sphinx-yellow-700/70"
             />
           ) : (
             <div className="flex  items-center   w-[80%]">
